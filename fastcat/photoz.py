36ad0c6985b7f645344641201b038d5edb90fc15
Anze Slosar <slosar@gmail.com>
2016-03-28 15:54:18 +0100

    Added interface for general photo-zs

--- fastcat/photoz.py
+++ fastcat/photoz.py
@@ -46,6 +46,33 @@ class PhotoZBase(object):
         pass
         return arr
 
+    def getMeanRMS (self,arr):
+        """ Returns mean and sqrt variance for 
+            the photoz pDF, given array
+            Note that for assymetric PZ, you are at your
+            own risk.
+        """
+        # in base class we return redshift and zero varinace.
+        N=len(arr)
+        return arr["z"],np.zeros(N)
+
+    def getMinMax(self,arr):
+        """ Returns range of redshifts where p(z) is considerable, i.e.
+        no real probability at z<zmin or z>zmax.
+        Note that in case of catastrophic outliers, one can have considerable
+        amounts of zeros in this range
+        """
+        return arr["z"], arr["z"]
+
+    def PofZ(self,arr,z,dz):
+        """ Returns probability of PZ be at z +-dz/2"""
+        N=len(arr)
+        P=zeros(N)
+        dzo2=dz/2
+        P[np.where(abs(arr["z"]-z)<dzo2)]=1.0
+        return P
+                    
+    
 class PhotoZGauss(PhotoZBase):
     """
     Idealised Gaussian PhotoZs
@@ -77,3 +104,26 @@ class PhotoZGauss(PhotoZBase):
         arr['z']=arr['z']+(1.+arr['z'])*np.random.normal(0.,self.sigma)
         return arr
         
+    def getMeanRMS (self,arr):
+        """ Returns mean and sqrt variance for 
+            the photoz pDF, given array. 
+            Note that for assymetric PZ, you are at your
+            own risk.
+        """
+        return arr["z"],self.sigma*(1+arr["z"])
+
+    def getMinMax(self,arr):
+        """ Returns range of redshifts where p(z) is considerable, i.e.
+        no real probability at z<zmin or z>zmax.
+        Note that in case of catastrophic outliers, one can have considerable
+        amounts of zeros in this range
+        """
+        return arr["z"]-self.sigma*(1+arr["z"])*5,arr["z"]+self.sigma*(1+arr["z"])*5
+
+    def PofZ(self,arr,z,dz):
+        """ Returns probability of PZ be at z +-dz/2"""
+        sig=self.sigma*(1+arr["z"])
+        norm=1./np.sqrt(2*np.pi)/sig
+        return np.exp(-(arr["z"]-z)**2/(2*self.sigma**2))*norm*dz
+    
+    
